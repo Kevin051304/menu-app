09478f611753c6e220a78a325a85567bf8dacd92
Kevin Lin <linkevin644931@gmail.com>
2023-12-17 17:28:09 -0500

    updated the state of appetizer so changes made by users persist

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:menu/appetizer_state.dart';
 import 'package:menu/pages/welcome.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Clear the persisted state during app initialization
+  await clearPersistedState();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppetizerState()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+Future<void> clearPersistedState() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.remove('appetizer_quantities');
+  prefs.remove('appetizer_isFavorited');
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +32,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'Poppins'),
-        home: const Welcome());
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const Welcome(),
+    );
   }
 }
